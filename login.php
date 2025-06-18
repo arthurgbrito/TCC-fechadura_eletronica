c3a3bde11d074d8ab6806b7c803f08e2a409f163
Arthur Gessler de Brito <arthurgesslerdebrito@gmail.com>
2025-06-17 21:25:22 -0300

    alterando o banco de dados para o xampp

--- login.php
+++ login.php
@@ -1,23 +1,23 @@
 <?php 
 
-$host = "db.atjkpeoxwzdvuofvecru.supabase.co"; // sem https
-$dbname = "postgres";
-$user = "postgres";
-$password = "Agb.191672@_";
-$port = "5432";
+$host = "localhost";
+$dbname = "banco-teste-tcc";
+$user = "root";
+$password = "";
+$port = "3306";
 
 try {
-    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Conectado com sucesso!";
+     print "Conectado no banco de dados com sucesso!";
 } catch (PDOException $e){
-    die("Erro ao acessar o banco de dados: " . $e->getMessage());
+    echo "Erro ao acessar o banco de dados: " . $e->getMessage();
 }
-
-$username = $_POST['Username'];
+/*
+$username = $_POST['user'];
 $pass = $_POST['password'];
 
-// Consulta com prepared statement (evita SQL Injection)
+
 $stmt = $conn->prepare("SELECT * FROM Usuarios WHERE Username = :username AND Password = :pass");
 $stmt->bindParam(':username', $username);
 $stmt->bindParam(':pass', $pass);
@@ -28,5 +28,5 @@ if ($stmt->rowCount() > 0) {
     // header("Location: painel.php");
 } else {
     echo "Email ou senha inválidos.";
-}
+}*/
 ?>
